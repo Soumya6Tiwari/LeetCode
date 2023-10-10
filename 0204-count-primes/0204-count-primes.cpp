10a822139ae64a66ef66b6561e2c5b37082c953c
soumya tiwari <soumyaecc6@gmail.com>
2023-10-10 17:19:22 +0530

    Time: 573 ms (30.58%), Space: 10.6 MB (32.62%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int countPrimes(int n) 
+    {
+        if(n<=2)
+        {
+            return 0;
+        }
+         int count=0;
+        vector<bool>array(n,false);
+        for(int i=2;i<=sqrt(n);i++)
+        {
+            if(array[i]==false)
+            {
+                for(int j=i*2;j<n;j+=i)
+                {
+                    array[j]=true;
+                }
+            }
+        }
+        
+        for(int i=2;i<n;i++)
+        {
+            if(array[i]==false)
+            {
+                count++;
+            }
+        }
+        return count;
+       
+        
+     
+}
+    
+};
