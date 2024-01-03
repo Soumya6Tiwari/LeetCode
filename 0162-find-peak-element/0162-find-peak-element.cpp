cae9ccf5d359b07828e2caf94a713912ac5fb1fa
soumya tiwari <soumyaecc6@gmail.com>
2024-01-04 01:42:59 +0530

    Time: 6 ms (35.79%), Space: 9.2 MB (73.76%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     int findPeakElement(vector<int>& nums)
     {
-        int i,j,maxi=INT_MIN,ans,sum=0;
+        int i,j,maxi=INT_MIN,ans,sum=0,val;
         
         if(nums.size()==1)
         {
             return 0;
         }
-        if(nums.size()<=7)
-        {
+        
+    
             for(i=0;i<nums.size();i++)
             {
                 if(nums[i]>maxi)
@@ -18,9 +18,12 @@ public:
                     ans=i;
                 }
             }
+        if(maxi==nums[0])
+        {
             return ans;
-            
         }
+            
+        
         for(i=1;i<nums.size()-1;i++)
         {
             if(nums[i]>nums[i-1]&&nums[i]>nums[i+1])
