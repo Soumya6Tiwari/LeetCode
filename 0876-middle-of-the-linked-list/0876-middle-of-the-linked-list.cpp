5e3c72cb8bd726888fcecef3669d222a0402e169
soumya tiwari <soumyaecc6@gmail.com>
2024-01-19 00:37:26 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (10.75%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
@@ -12,58 +12,53 @@ class Solution {
 public:
     ListNode* middleNode(ListNode* head) 
     {
-        int length=0,mid,flag=0;
-        ListNode*temp=new ListNode();
-        ListNode*result=new ListNode();
-        ListNode*head1=new ListNode();
-        ListNode*temp1=new ListNode();
         
+          // NORMAL METHOD
         
-        temp=head;
-        while(temp!=NULL)
-        {
-            length++;
-            temp=temp->next;
-        }
-    
-        
-          mid=(length/2)+1;
-        temp=head;
-        length=0;
-        while(temp!=NULL)
-        {
-            length++;
-            if(length==mid)
-            {
-                head=temp;
-                break;
-                             
-            }
-            temp=temp->next;
-        }
-        temp1=head;
-//         while(temp1!=NULL)
+//         int length=0,mid,flag=0;
+//         ListNode*temp=new ListNode();
+//         ListNode*temp1=new ListNode();
+//         temp=head;
+//         while(temp!=NULL)
 //         {
-//             ListNode*result=new ListNode();
-//             if(flag==0)
+//             length++;
+//             temp=temp->next;
+//         }
+//         mid=(length/2)+1;
+//         temp=head;
+//         length=0;
+//         while(temp!=NULL)
+//         {
+//             length++;
+//             if(length==mid)
 //             {
-                
-//                 result->val=temp->val;
-//                 head=result;
-//                 temp1=head;
-//                 flag=1;      
-//             }
-//             else
-//             {
-                
-//                 result->val=temp->val;
-//                 temp1->next=result;
-//                 temp1=result;
+//                 head=temp;
+//                 break;
+                             
 //             }
 //             temp=temp->next;
 //         }
-//         return result;
-        return temp1;
+//         temp1=head;
+
+//         return temp1;
+        
+        
+        
+        // USING TORTOISE ALGORITHM ie TWO POINTERS GAME 
+        ListNode*fast=new ListNode();
+        ListNode*slow=new ListNode();
+        fast=head;
+        slow=head;
+        while(fast!=NULL)
+        {
+            fast=fast->next;
+            if(fast!=NULL)
+            {
+                fast=fast->next;
+                slow=slow->next;
+            }
+        }
+        return slow;
         
     }
 };
