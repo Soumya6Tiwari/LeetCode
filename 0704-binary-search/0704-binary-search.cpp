f5e6d2fb345de40844de0fad987c72bf96630012
Soumya Tiwari <soumyaecc6@gmail.com>
2024-02-18 22:03:17 +0530

    Time: 27 ms (67.06%), Space: 30 MB (11.86%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) 
+    {
+        int i,j,k;
+        for(i=0;i<nums.size();i++)
+        {
+            if(nums[i]==target)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
