f8aec4e2dffc9f1126ce9714a471f2caf05f2edd
Soumya Tiwari <soumyaecc6@gmail.com>
2024-06-24 12:12:55 +0530

    Time: 8 ms (39.24%), Space: 17.4 MB (45.36%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -13,47 +13,74 @@ class Solution {
 public:
     int maxDepth(TreeNode* root)
     {
-        //  always check this
-        if(root==NULL)
-        {
-            return 0;
-        }
-        // USING LEVEL ORDER TRAVERSAL
-         queue<TreeNode*>q;
-         int count=0;
-        q.push(root);
-        q.push(NULL);
-        count++;
-        while(q.size()>1)
-        {
-            TreeNode* front=q.front();
-            //case 1: if front is valid node
-            if(front!=NULL)
-            {
-                if(front->left)
-                {
-                    q.push(front->left);
-                }
-                if(front->right)
-                {
-                    q.push(front->right);
-                }
+        
+        //USING RECURSION
+        
+         // base case
+         if(root==NULL)
+         {
+             return 0;
+         }
+         int leftHeight=maxDepth(root->left);
+         int rightHeight=maxDepth(root->right);
+         return max(leftHeight,rightHeight)+1;
+         
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+//         //  always check this
+//         if(root==NULL)
+//         {
+//             return 0;
+//         }
+//         // USING LEVEL ORDER TRAVERSAL
+//          queue<TreeNode*>q;
+//          int count=0;
+//         q.push(root);
+//         q.push(NULL);
+//         count++;
+//         while(q.size()>1)
+//         {
+//             TreeNode* front=q.front();
+//             //case 1: if front is valid node
+//             if(front!=NULL)
+//             {
+//                 if(front->left)
+//                 {
+//                     q.push(front->left);
+//                 }
+//                 if(front->right)
+//                 {
+//                     q.push(front->right);
+//                 }
                 
-                q.pop();
-            }
-            else
-            {
-                  if(q.size()==1)
-                  {
-                      break;
-                  }
-                    q.push(NULL);
-                    count++;
-                    q.pop();
+//                 q.pop();
+//             }
+//             else
+//             {
+//                   if(q.size()==1)
+//                   {
+//                       break;
+//                   }
+//                     q.push(NULL);
+//                     count++;
+//                     q.pop();
                 
-            }
+//             }
             
-        }
-        return count;
+//         }
+//         return count;
     }
 };
