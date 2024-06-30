8de31f0fe6b057b1a66686f9ecd1081f8f2426df
Soumya Tiwari <soumyaecc6@gmail.com>
2024-07-01 02:31:05 +0530

    Time: 16 ms (65.50%), Space: 33.8 MB (88.81%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cpp
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -2,47 +2,76 @@ class Solution {
 public:
     int pivotIndex(vector<int>& nums) 
     {
-        int i,j,k,lsum=0,rsum=0;
+//         int i,j,k,lsum=0,rsum=0;
+//         for(i=0;i<nums.size();i++)
+//         {
+//                 if(i==0)
+//                 {
+//                     lsum=0;
+//                 }
+//                 else
+//                 {
+//                     lsum=lsum+nums[i-1];
+//                 }
+//                 if(i==nums.size()-1)
+//                 {
+//                     rsum=0;
+//                 }
+//                 else
+//                 {
+//                     j=i+1;
+//                     while(j!=nums.size())
+//                     {    
+//                     rsum=rsum+nums[j];
+//                         j++;
+//                     }
+                
+//                 }
+            
+            
+//             if(lsum==rsum)
+//             {
+//                 break;
+//             }
+//             else
+//             {
+//                 rsum=0;
+//             }
+//         }
+//         if(i==nums.size())
+//         {
+//             return -1;
+//         }
+//         return i;
+        
+        
+        int i,j,k,sum=0,lsum=0,rsum=0;
         for(i=0;i<nums.size();i++)
         {
-                if(i==0)
-                {
-                    lsum=0;
-                }
-                else
-                {
-                    lsum=lsum+nums[i-1];
-                }
-                if(i==nums.size()-1)
-                {
-                    rsum=0;
-                }
-                else
-                {
-                    j=i+1;
-                    while(j!=nums.size())
-                    {    
-                    rsum=rsum+nums[j];
-                        j++;
-                    }
-                
-                }
-            
-            
-            if(lsum==rsum)
+            sum=sum+nums[i];
+        }
+        for(i=0;i<nums.size();i++)
+        {
+            if(i==0)
             {
-                break;
+                lsum=0;
+                rsum=sum-nums[i]-lsum;
+            }
+            else if(i==nums.size()-1)
+            {
+                rsum=0;
+                lsum=sum-rsum-nums[i];
             }
             else
             {
-                rsum=0;
+                lsum=lsum+nums[i-1];
+                rsum=sum-lsum-nums[i];
+            }
+            if(lsum==rsum)
+            {
+                return i;
             }
         }
-        if(i==nums.size())
-        {
-            return -1;
-        }
-        return i;
-        
+        return -1;
     }
 };
