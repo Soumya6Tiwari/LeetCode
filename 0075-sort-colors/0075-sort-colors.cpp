724c2e11d37b7e7eeca58470134b6e58c193110b
Soumya Tiwari <soumyaecc6@gmail.com>
2024-07-15 22:41:26 +0530

    Time: 0 ms (100.00%), Space: 10.2 MB (8.55%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -2,27 +2,97 @@ class Solution {
 public:
     void sortColors(vector<int>& nums)
     {
-       // DUTCH NATIONAL FLAG ALGORITHM (STRIVER'S SHEET)
-        int low=0,mid=0,high=nums.size()-1,i,j,k;
+        // Three pointer approach
+        int i,j,k,temp;
+        int low=0,mid=0,high=nums.size()-1;
         while(mid<=high)
         {
-            if(nums[mid]==0)
+            if(nums[mid]==1)
             {
-                swap(nums[mid],nums[low]);
-                 mid++;
-                 low++;
+                mid++;
             }
-            else if(nums[mid]==1)
+            else if(nums[mid]==0)
             {
-                  mid++;   
+                temp=nums[mid];
+                nums[mid]=nums[low];
+                nums[low]=temp;
+                mid++;
+                low++;
             }
-            else
+            else if(nums[mid]==2)
             {
-                 swap(nums[mid],nums[high]);
-                 high--;
+                temp=nums[mid];
+                nums[mid]=nums[high];
+                nums[high]=temp;
+                high--;
             }
         }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+//        // DUTCH NATIONAL FLAG ALGORITHM (STRIVER'S SHEET)
+//         int low=0,mid=0,high=nums.size()-1,i,j,k;
+//         while(mid<=high)
+//         {
+//             if(nums[mid]==0)
+//             {
+//                 swap(nums[mid],nums[low]);
+//                  mid++;
+//                  low++;
+//             }
+//             else if(nums[mid]==1)
+//             {
+//                   mid++;   
+//             }
+//             else
+//             {
+//                  swap(nums[mid],nums[high]);
+//                  high--;
+//             }
+//         }
       
+        
+        
+        
+        
         
     }
 };
