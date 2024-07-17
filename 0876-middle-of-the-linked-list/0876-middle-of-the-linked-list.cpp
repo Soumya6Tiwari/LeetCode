09c77b49180ddb52c876f1df7ab19e813d68045f
Soumya Tiwari <soumyaecc6@gmail.com>
2024-07-17 23:16:15 +0530

    Time: 4 ms (34.46%), Space: 9.2 MB (5.95%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
@@ -10,55 +10,26 @@
  */
 class Solution {
 public:
-    ListNode* middleNode(ListNode* head) 
+    ListNode* middleNode(ListNode* head)
     {
-        
-          // NORMAL METHOD
-        
-//         int length=0,mid,flag=0;
-//         ListNode*temp=new ListNode();
-//         ListNode*temp1=new ListNode();
-//         temp=head;
-//         while(temp!=NULL)
-//         {
-//             length++;
-//             temp=temp->next;
-//         }
-//         mid=(length/2)+1;
-//         temp=head;
-//         length=0;
-//         while(temp!=NULL)
-//         {
-//             length++;
-//             if(length==mid)
-//             {
-//                 head=temp;
-//                 break;
-                             
-//             }
-//             temp=temp->next;
-//         }
-//         temp1=head;
-
-//         return temp1;
-        
-        
-        
-        // USING TORTOISE ALGORITHM ie TWO POINTERS GAME 
-        ListNode*fast=new ListNode();
-        ListNode*slow=new ListNode();
-        fast=head;
-        slow=head;
-        while(fast!=NULL)
+        int length=0,mid,i,j,k,ans;
+        ListNode*temp=head;
+        while(temp!=NULL)
         {
-            fast=fast->next;
-            if(fast!=NULL)
-            {
-                fast=fast->next;
-                slow=slow->next;
-            }
+            length++;
+            temp=temp->next;
+            
         }
-        return slow;
+        ans=(length/2)+1;
+        length=0;
+        temp=head;
+        while(length<ans-1)
+        {
+            length++;
+            temp=temp->next;
+            
+        }
+        return temp;
         
     }
 };
