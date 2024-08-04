f4e3f64f8cfb27f68021c254e3e9da8dc30fa6c7
Soumya Tiwari <soumyaecc6@gmail.com>
2024-08-05 01:12:58 +0530

    Time: 100 ms (21.64%), Space: 27.1 MB (38.96%) - LeetHub

--- 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.cpp
+++ 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.cpp
@@ -1,53 +1,24 @@
 class Solution {
 public:
-//     void calculate(vector<int>&nums,int n,int &sum, int index,vector<int>&v,vector<int>&v1)
-//     {
-//         if(index==n)
-//         {
-//             v.push_back(sum);
-//             return;
-//         }
-        
-//         // pickup case
-//         v1.push_back(nums[index]);
-//         sum=sum+nums[index];
-//         calculate(nums,n,sum,index+1,v,v1);
-        
-//         // not pickup case
-//         v1.pop_back();
-//         sum=sum-nums[index];
-//         calculate(nums,n,sum,index+1,v,v1);     
-//     }
-    int rangeSum(vector<int>& nums, int n, int left, int right)
+    int rangeSum(vector<int>& nums, int n, int left, int right) 
     {
-//         vector<int>v,v1;
-//         int i,j,k;
-//         int sum=0,index=0,val=0;
-//         calculate(nums,n,sum,index,v,v1);
-        
-//         sort(v.begin(),v.end());
-        
-//         for(i=left;i<=right;i++)
-//         {
-//             val=val+v[i];
-//         }
-//         // return val%1000000007;
-//         return v[10];
-        vector<int> arr;
-        for (int i = 0; i < n; ++i) {
-            int prefix = 0;
-            for (int j = i; j < n; ++j) {
-                prefix += nums[j];
-                arr.push_back(prefix);
+        vector<int>res,v1,ans;
+        long long i,j,k,sum,val=0;
+        for(i=0;i<n;i++)
+        {
+            for(j=i;j<n;j++)
+            {
+                sum=sum+nums[j];
+                res.push_back(sum);
             }
+            
+            sum=0;
         }
-        sort(arr.begin(), arr.end());
-        int result = 0;
-        int MOD = 1000000007;
-        for (int i = left - 1; i < right; ++i) {
-            result = (result + arr[i]) % MOD;
+        sort(res.begin(),res.end());
+        for(i=left-1;i<right;i++)
+        {
+            val=(val+res[i])%1000000007;
         }
-        return result;
-        
+        return (int)val;
     }
 };
