afacefcb546962f037b0ea161bc223f9faa0d04a
Soumya Tiwari <soumyaecc6@gmail.com>
2024-08-08 01:30:26 +0530

    Time: 22 ms (45.37%), Space: 27.5 MB (64.25%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -2,15 +2,27 @@ class Solution {
 public:
     void rotate(vector<int>& nums, int k) 
     {
-        int i,j;
-        vector<int>v(nums.size(),-1);
-        for(i=0;i<nums.size();i++)
-        {
-            v[(i+k)%nums.size()]=nums[i];
-        }
-        for(i=0;i<v.size();i++)
-        {
-            nums[i]=v[i];
-        }
+        // BRUTE FORCE APPROACH
+        
+        // int i,j;
+        // vector<int>v(nums.size(),-1);
+        // for(i=0;i<nums.size();i++)
+        // {
+        //     v[(i+k)%nums.size()]=nums[i];
+        // }
+        // for(i=0;i<v.size();i++)
+        // {
+        //     nums[i]=v[i];
+        // }
+        
+        // OPTIMAL APPROACH
+        int n=nums.size();
+        k=k%n;
+        
+        reverse(nums.begin(),nums.end());
+        reverse(nums.begin(),nums.begin()+k);
+        reverse(nums.begin()+k,nums.end());
+        
+        
     }
 };
