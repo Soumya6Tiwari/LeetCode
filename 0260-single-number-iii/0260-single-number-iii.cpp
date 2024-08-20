7abb5ff651b5881b119f803a57a0e37664e1f3b6
Soumya Tiwari <soumyaecc6@gmail.com>
2024-08-20 15:49:35 +0530

    Time: 12 ms (14.61%), Space: 14.6 MB (13.73%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.cpp
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -2,33 +2,55 @@ class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) 
     {
-        vector<int>result;
+         // SELF THOUGHT PROCESS
+        
+        // vector<int>result;
+        // vector<int>ans;
+        //  vector<int>ans1;
+        // set<int>s;
+        // int i,j,k,num,val,count=0;
+        // for(i=0;i<nums.size();i++)
+        // {
+        //     s.insert(nums[i]);
+        // }
+        // result.assign(s.begin(),s.end());
+        // ans=result;
+        // for(i=0;i<result.size();i++)
+        // {
+        //     for(j=0;j<nums.size();j++)
+        //     {
+        //         if(result[i]==nums[j])
+        //         {
+        //             count++;
+        //         }
+        //     }
+        //     if(count==1)
+        //     {
+        //         ans1.push_back(result[i]);
+        //     }
+        //     count=0;
+        // }
+        // return ans1;
+        
+        
+        //  USING THE MAP
+        map<int, int>m;
         vector<int>ans;
-         vector<int>ans1;
-        set<int>s;
-        int i,j,k,num,val,count=0;
+        int i,j,k;
         for(i=0;i<nums.size();i++)
         {
-            s.insert(nums[i]);
+            m[nums[i]]++;
         }
-        result.assign(s.begin(),s.end());
-        ans=result;
-        for(i=0;i<result.size();i++)
+        for(auto it:m)
         {
-            for(j=0;j<nums.size();j++)
+            if(it.second==1)
             {
-                if(result[i]==nums[j])
-                {
-                    count++;
-                }
+                ans.push_back(it.first);
+                
+
             }
-            if(count==1)
-            {
-                ans1.push_back(result[i]);
-            }
-            count=0;
         }
-        return ans1;
+        return ans;
         
     }
 };
